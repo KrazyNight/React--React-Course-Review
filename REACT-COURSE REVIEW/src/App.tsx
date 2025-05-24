074b2e1994d432aca180d0284613bf8cc763d1a8
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2025-05-23 19:31:17 -0700

    react finished basic css

---
+++ REACT-COURSE REVIEW/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Todo from "./components/Todo.jsx";
+import TodoTitle from './components/TodoTitle.jsx';
+import Popup from './components/Popup.jsx';
+
+function App() {
+
+  return (
+    <>
+      <TodoTitle></TodoTitle>
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      {/* <Popup /> */}
+
+    </>
+  )
+}
+
+export default App
---
+++ REACT-COURSE REVIEW/src/components/Todo.jsx
@@ -0,0 +1,10 @@
+function Todo() {
+    return (
+        <div className="todo-item">
+            <h2>Finish React Crash Course</h2>
+            <button>Delete</button>
+        </div>
+    )
+}
+
+export default Todo;
